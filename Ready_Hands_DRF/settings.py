4876d5a1606c4571af96db3ed5cff3ed3687d053
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-05-06 23:24:07 +0300

    accept added

--- Ready_Hands_DRF/settings.py
+++ Ready_Hands_DRF/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pusj@ju4y118z8)8ks@ykipu!!ai(juqs+)#n_ifq6q*!hn03='
 
 # SECURITY WARNING: don't run with debug turned on in production!64.227.125.47
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['64.227.125.47']
 
 
 # Application definition
