71e26f9e68b233e6523a0bc138426209660abb4b
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-05-08 05:34:44 +0300

    cross HTTPS

--- Ready_Hands_DRF/settings.py
+++ Ready_Hands_DRF/settings.py
@@ -146,7 +146,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+CORS_ORIGIN_WHITELIST = [
+    "https://www.readyhands.site",
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 CORS_ORIGIN_ALLOW_ALL = True
